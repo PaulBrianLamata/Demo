9d675898701d29151d547efd5b806fbaa9a5639e
Paul <79885067+PaulBrianLamata@users.noreply.github.com>
2025-09-09 11:05:15 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,21 @@
 module.exports = {
+  parser: "@babel/eslint-parser",
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"]
+    }
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings"
-  ],
-  plugins: ["react", "import"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["react"],
   env: {
     es6: true,
-    "shared-node-browser": true
-  },
-  settings: {
-    react: { version: "detect" },
-    "import/resolver": {
-      node: true
-    }
-  },
-  globals: {
-    global: true,
-    process: true
+    node: true,
+    browser: true,
+    jest: true
   },
   rules: {
-    "no-console": "off"
-  },
-  overrides: [
-    {
-      files: [
-        ".eslintrc.js",
-        "jest.config.js",
-        "jest.framework-setup.js",
-        "web/next.config.js",
-        "server/**/*.js"
-      ],
-      env: {
-        node: true
-      }
-    },
-    {
-      files: ["**/*.test.js"],
-      env: {
-        jest: true
-      }
-    },
-    {
-      files: [
-        "web/components/**/*.js",
-        "web/pages/**/*.js",
-        "web/mocks/**/*.js"
-      ],
-      rules: {
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error"
-      }
-    },
-    {
-      files: ["**/__fixtures__/**/*.js"]
-    }
-  ]
+    "no-console": "off",
+    "react/prop-types": "off"
+  }
 };
