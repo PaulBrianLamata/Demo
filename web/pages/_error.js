f0317018aabd5b31fdfc70e90a64ac5f3fa83b36
Paul <79885067+PaulBrianLamata@users.noreply.github.com>
2025-09-09 11:26:36 +0800

    Update _error.js

--- web/pages/_error.js
+++ web/pages/_error.js
@@ -1,26 +1,12 @@
-// @flow
-
 import React, { Component } from 'react';
 import withRedux from 'next-redux-wrapper';
 import { createStore } from '../store';
 import Layout from '../components/Layout';
 import Error from '../components/pages/Error';
 
-type Props = {
-  statusCode: ?number
-};
-
-type InitialProps = {
-  res?: http$ServerResponse,
-  err?: {
-    statusCode: number
-  }
-};
-
-export class ErrorPage extends Component<Props> {
-  static async getInitialProps({ res, err }: InitialProps) {
+export class ErrorPage extends Component {
+  static async getInitialProps({ res, err }) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-
     return { statusCode };
   }
 
