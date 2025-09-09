fcee858f495d04c49a5fe3ad451838d3bac46c6f
Paul <79885067+PaulBrianLamata@users.noreply.github.com>
2025-09-09 11:32:19 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ["@babel/preset-env", "@babel/preset-react"],
-    },
-  },
-  plugins: ["react"],
+  parser: '@babel/eslint-parser',
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "next", // ✅ adds Next.js rules
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'next' // Enables Next.js recommended rules
   ],
+  plugins: ['react'],
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
   settings: {
     react: {
-      version: "detect", // ✅ fixes React version warning
+      version: 'detect',
     },
   },
   rules: {
-    "no-console": "off",
+    // ✅ Disable rules causing noise after removing Flow
+    'react/prop-types': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+
+    // ✅ If you don’t want to enforce semicolons
+    'semi': ['error', 'never'], // or "always" if you prefer semicolons
+
+    // ✅ Optional: sometimes `import/no-unresolved` is too strict
+    'import/no-unresolved': 'off',
   },
-};
+}
